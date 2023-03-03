9e9cea7eed5de0d182eb0791cfbb9d5a7aaec804
priohelp <priohelp@prio.co.kr>
2023-03-03 14:01:49 +0900

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'priohelp', // Usually your GitHub org/user name.
   projectName: 'otoo-help-default', // Usually your repo name.
-
+  trailingSlash: false,
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   
